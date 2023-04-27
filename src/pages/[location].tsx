bd45601f9629b43ae588ab218de8cdd7a78aefa6
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2023-04-27 20:17:24 +0000

    Update [location].tsx and index.tsx

--- src/pages/[location].tsx
+++ src/pages/[location].tsx
@@ -2,7 +2,7 @@ import { Global } from '@emotion/react'
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-//import Head from 'next/head'
+import Head from 'next/head'
 import { getPortalsData } from 'lib/client/utils'
 
 export default function LocationSite() {
@@ -11,9 +11,9 @@ export default function LocationSite() {
 
   return (
     <>
-      {/*<Head>
+      <Head>
         <title>{location}</title>
-      </Head>*/}
+      </Head>
       <Global
         styles={{
           body: {
